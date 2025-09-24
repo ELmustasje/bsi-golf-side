0a576f1d2e3c0dd966e94afdb60899ba46c6a3b3
ELmustasje <thomasbarth@live.no>
2025-09-24 15:13:00 +0200

    u

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from 'react';
-import GolfSimulatorTable from './components/GolfSimulatorTable.tsx';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<GolfSimulatorTable />} />
-
-      </Routes>
-
-    </Router>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
